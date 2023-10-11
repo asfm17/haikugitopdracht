9c7b4f81ddb142d2aa60e09c2219e7c6c7b099ad
Akira <37931@ma-web.nl>
2023-10-11 09:31:15 +0200

    Boat

--- haiku.js
+++ haiku.js
@@ -3,7 +3,12 @@
 //plak hier de haiku functions onder elkaar
 
 
-
+function Haiku1(){
+    console.log("haiku by:","Docent");
+    console.log("Boat on water");
+    console.log("Big and violent waves");
+    console.log("Boat slowly sinks");
+}
 
 //laat de code hieronder met rust
 Haiku1()
