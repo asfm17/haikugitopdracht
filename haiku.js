08e08a1f7c92870b27ce969501e0b935f1c17aaf
Kaj <37048@ma-web.nl>
2023-10-11 09:32:55 +0200

    2de Haiku toegevoegd

--- haiku.js
+++ haiku.js
@@ -4,10 +4,17 @@
 
 
 function Haiku1(){
-    console.log("haiku by:","Docent");
+    console.log("haiku by:","Akira");
     console.log("Boat on water");
     console.log("Big and violent waves");
     console.log("Boat slowly sinks");
+}
+
+function Haiku2(){
+    console.log("haiku by:","Kaj");
+    console.log("Apple on a tree");
+    console.log("Falls on the ground");
+    console.log("Apple rolls away");
 }
 
 //laat de code hieronder met rust
